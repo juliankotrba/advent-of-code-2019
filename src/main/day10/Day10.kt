73a5f8771be459e18a7d63947bdd60108dc83f85
Julian Kotrba <me@juliankotrba.xyz>
2019-12-11 16:50:31 +0100

    Add day 10 part 1 solution

---
+++ src/main/day10/Day10.kt
@@ -0,0 +1,86 @@
+package day10
+
+import parse
+import kotlin.math.PI
+import kotlin.math.atan
+import kotlin.math.atan2
+
+typealias Map = List<List<Char>>
+typealias Asteroid = Pair<Int, Int>
+
+fun main() {
+    val testinput = mutableListOf<List<Char>>()
+    """
+.#..##.###...#######
+##.############..##.
+.#.######.########.#
+.###.#######.####.#.
+#####.##.#.##.###.##
+..#####..#.#########
+####################
+#.####....###.#.#.##
+##.#################
+#####.##.###..####..
+..######..##.#######
+####.##.####...##..#
+.#####..#.######.###
+##...#.##########...
+#.##########.#######
+.####.#.###.###.#.##
+....##.##.###..#####
+.#.#.###########.###
+#.#.#.#####.####.###
+###.##.####.##.#..##
+            """.trimIndent().lines().forEach { line ->
+        testinput.add(line.toList())
+    }
+
+
+    val input = parse("src/main/day10/input") { lines ->
+        val rows = mutableListOf<List<Char>>()
+        lines.forEach { line ->
+            rows.add(line.toList())
+        }
+        rows.toList()
+    }
+
+    println(part1(input))
+
+}
+
+fun part1(input: List<List<Char>>): Int {
+
+    val asteroids = getAsteroids(input)
+    val visibleMap = mutableMapOf<Asteroid, Int>()
+
+    asteroids.forEach { selected ->
+        val s = mutableSetOf<Double>()
+        asteroids.forEach {
+            if (selected.first != it.first || selected.second != it.second) {
+                s.add(phi(selected, it))
+            }
+        }
+        visibleMap[selected] = s.size
+    }
+    print(visibleMap.maxBy { it.value }?.key)
+    return visibleMap.maxBy { it.value }?.value ?: 0
+}
+
+private fun phi(fromAsteroid: Asteroid, toAsteroid: Asteroid): Double =
+        atan2(
+                (toAsteroid.second - fromAsteroid.second).toDouble(),
+                (toAsteroid.first - fromAsteroid.first).toDouble()
+        )
+
+private fun getAsteroids(input: List<List<Char>>): List<Asteroid> {
+    val asteroids = mutableListOf<Asteroid>()
+
+    input.forEachIndexed { y, _ ->
+        input.forEachIndexed { x, _ ->
+            if (input[y][x] == '#') {
+                asteroids.add(Asteroid(x, y))
+            }
+        }
+    }
+    return asteroids
+}
